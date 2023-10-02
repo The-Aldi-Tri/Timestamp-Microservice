22abe8b89363be62fad48bcb3058abd5a64e3aea
The-Aldi-Tri <the.aldi.tri@gmail.com>
2023-10-02 12:54:31 +0700

    All test passed

---
+++ README.md
@@ -0,0 +1,13 @@
+# Timestamp Microservice
+Build a full stack JavaScript app that is functionally similar to this: https://timestamp-microservice.freecodecamp.rocks.
+
+## FreeCodeCamp - Back End Development and API Certification Projects
+
+1. Metric-Imperial Converter - 
+2. Request Header Parser Microservice - 
+3. URL Shortener Microservice - 
+4. Exercise Tracker - 
+5. File Metadata Microservice - 
+
+Instructions and challenges for building the project can be found at https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/timestamp-microservice.<br />
+The boilerplate for the project can be found at https://github.com/freeCodeCamp/boilerplate-project-timestamp/.
---
+++ index.js
@@ -0,0 +1,55 @@
+// index.js
+// where your node app starts
+
+// init project
+const express = require('express')
+const app = express()
+const path = require('path')
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+const cors = require('cors')
+app.use(cors({ optionsSuccessStatus: 200 })) // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' })
+})
+
+app.get('/api/', (req, res) => {
+  const currentUnixTime = Date.now()
+  const currentUtcTime = new Date().toUTCString()
+  res.json({ unix: currentUnixTime, utc: currentUtcTime })
+})
+
+app.get('/api/:date?', (req, res) => {
+  const dateString = req.params.date
+  const regex = /^\d+$/
+  const isOnlyNumber = regex.test(dateString)
+  if (isOnlyNumber) {
+    const unixTime = parseInt(dateString)
+    const utcTime = new Date(parseInt(dateString)).toUTCString()
+    res.json({ unix: unixTime, utc: utcTime })
+  } else {
+    const unixTime = Date.parse(dateString)
+    if (unixTime) {
+      const utcTime = new Date(unixTime).toUTCString()
+      res.json({ unix: unixTime, utc: utcTime })
+    } else {
+      res.json({ error: 'Invalid Date' })
+    }
+  }
+})
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port)
+})
